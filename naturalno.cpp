5f5a1ddf840902369693a986651d02fae3c3c68f
prateek2421 <prateekkumar3628@gmail.com>
2022-04-03 22:48:59 +0530

    stagged

---
+++ naturalno.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+   int n=5, sum=0, i;
+   for(i=1;i<=n;i++)
+   sum=sum+i;
+   cout<<"Sum of first "<<n<<" natural numbers is "<<sum;
+   return 0;
+}
